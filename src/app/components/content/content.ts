ba3efc9c1a5d1520e553b68c4cee9a69756d385b
VeneziaDesigns <adrianhuartef@gmail.com>
2025-06-17 01:58:02 +0200

    porfolio v1

--- src/app/components/content/content.ts
+++ src/app/components/content/content.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 import { Profile } from "../profile/profile";
 import { Experience } from "../experience/experience";
 import { Projects } from "../projects/projects";
-import { Testimonials } from "../testimonials/testimonials";
 import { Education } from "../education/education";
-import { Articles } from "../articles/articles";
-import { Subscribe } from "../subscribe/subscribe";
+import { Skills } from '../skills/skills';
 
 @Component({
   selector: 'app-content',
-  imports: [Profile, Experience, Projects, Testimonials, Education, Articles, Subscribe],
+  imports: [Profile, Experience, Projects, Education, Skills],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
